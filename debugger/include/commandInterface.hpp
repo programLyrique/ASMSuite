c8ac84a505f9861e42638f02bf07542b6acbef53
Pierre Donat-bouillud <pdonatbo@laplace>
2013-03-26 14:52:30 +0100

    Ajout des fichiers qui décrivent une interface (console, ou graphique).

---
+++ debugger/include/commandInterface.hpp
@@ -0,0 +1,17 @@
+#ifdef COMMAND_INTERFACE_H
+#define COMMAND_INTERFACE_H
+
+namespace debugger
+{
+
+/**
+ * L'interface par laquelle l'utilisateur interragit avec le debogueur.
+ */
+class CommandInterface
+{
+    virtual 
+};
+
+}
+
+#define COMMAND_INTERFACE_H
---
+++ debugger/src/commandInterface.cpp
@@ -0,0 +1,6 @@
+#include "commandInterface.hpp"
+
+namespace debugger
+{
+
+}
