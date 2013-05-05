6aa501b145cbe1c1fb69d7926a901b526abe7688
programLyrique <program@Lyrique.org>
2013-05-06 00:04:21 +0200

    Ajout de getters pour le CPU.

--- simulator/include/cpu.hpp
+++ simulator/include/cpu.hpp
@@ -30,13 +30,39 @@ class CPU {
             registers = new Registers(&bus_in, &bus_out1, &bus_out2);
             data_memory = new Memory(&bus_out1, &bus_out2, &bus_in);
             program_memory = new Memory(&bus_pc, &pm_in, &bus_inst);
-            io = new IO(bus_in, busèout1, bus_out2);
+            io = new IO(&bus_in, &bus_out1, &bus_out2);
             control_unit = new Control_unit(&bus_pc, &bus_inst, &bus_out2,
                           &flag_z, &flag_p, &flag_n, alu, data_memory,
                           program_memory, registers, io);
         }
 
         void read_program (char* filename);
+        
+        Memory* getDataMemory() const
+        { 
+            return data_memory;
+        }
+        
+        Control_unit* getControl_Unit() const
+        {
+            return control_unit;
+        }
+        
+        IO* getIO() const
+        {
+            return io;
+        }
+        
+        Registers* getRegisters() const
+        {
+            return registers;
+        }
+        
+        ALU* getALU() const
+        {
+            return alu;
+        }
+        
 };
 
 #endif
