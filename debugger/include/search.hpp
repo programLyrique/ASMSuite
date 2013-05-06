d9f91502be842fbea6363f163a93f8aa77591864
programLyrique <program@Lyrique.org>
2013-05-06 18:32:05 +0200

    Interface de recherche terminée.

--- debugger/include/search.hpp
+++ debugger/include/search.hpp
@@ -8,17 +8,51 @@
 #ifndef SEARCH_HPP
 #define	SEARCH_HPP
 
+#include <cstdint>
+
+class Memory;
+
 namespace debugger
 {
-
+/**
+ * Pour effectuer des recherches dans la mémoire.
+ * @param mem
+ */
 class Search
 {
 public:
-    Search();
+    Search(Memory& mem);
     Search(const Search& orig);
+    
+    /**
+     * Lancer une nouvelle recherche
+     * @param seq
+     * @param size_seq
+     */
+    void new_search(int32_t* seq, int size_seq);
+    
+    /**
+     * Lancer une nouvelle recherche limitée à l'intervalle 
+     * d'adresse [debut, debut + offset [
+     * @param seq
+     * @param size_seq
+     * @param debut
+     * @param fin
+     */
+    void new_search(int32_t* seq, int size_seq, int debut, int offset);
+    /**
+     * Renvoie la prochaine adresse qui correspond à la séquence
+     * @return -1 s'il n'y a plus rien
+     */
+    int find_next();
     virtual ~Search();
 private:
-
+    const Memory& mem;
+    std::int32_t* seq;
+    int size_seq;
+    int current;
+    int debut;
+    int fin;
 };
 
 }
--- debugger/src/search.cpp
+++ debugger/src/search.cpp
@@ -8,15 +8,34 @@
 #include "search.hpp"
 
 
+using namespace std;
+
 namespace debugger
 {
 
-    Search::Search()
+    Search::Search(Memory& mem) : mem(mem), current(0)
     {
     }
 
-    Search::Search(const Search& orig)
+    void Search::new_search(int32_t* seq, int size_seq)
     {
+        current = 0;
+        //mettre fin à la taille de la mémoire
+        this->seq = seq;
+        this->size_seq = size_seq;
+    }
+    
+    void Search::new_search(int32_t* seq, int size_seq, int debut, int offset)
+    {
+        current = debut;
+        fin =debut + offset;
+        this->seq=seq;
+        this->size_seq = size_seq;
+    }
+    
+    int Search::find_next()
+    {
+        return current;
     }
 
     Search::~Search()
