efa60196416ba109d799e82586b98fca588d042d
programLyrique <program@Lyrique.org>
2013-05-06 18:06:59 +0200

    Recherche dans la mémoire.

---
+++ debugger/include/search.hpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   Search.hpp
+ * Author: pierre
+ *
+ * Created on 6 mai 2013, 18:05
+ */
+
+#ifndef SEARCH_HPP
+#define	SEARCH_HPP
+
+namespace debugger
+{
+
+class Search
+{
+public:
+    Search();
+    Search(const Search& orig);
+    virtual ~Search();
+private:
+
+};
+
+}
+#endif	/* SEARCH_HPP */
+
---
+++ debugger/src/search.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   Search.cpp
+ * Author: pierre
+ * 
+ * Created on 6 mai 2013, 18:05
+ */
+
+#include "search.hpp"
+
+
+namespace debugger
+{
+
+    Search::Search()
+    {
+    }
+
+    Search::Search(const Search& orig)
+    {
+    }
+
+    Search::~Search()
+    {
+    }
+
+}
